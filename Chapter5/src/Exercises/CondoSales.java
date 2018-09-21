1f034bb1f5ee575274777e176f6701a2e2959447
Jordan Elliott <37377660+YourTechie@users.noreply.github.com>
2018-09-21 09:54:39 -0500

    Did exercises

--- Chapter5/src/Exercises/CondoSales.java
+++ Chapter5/src/Exercises/CondoSales.java
@@ -8,28 +8,31 @@ public class CondoSales
 	{
 		int choice;
 		
+		
+		
 		String result;
 		
 		Scanner input = new Scanner(System.in);
-
-		System.out.println("Pick a number 1-3");
-		choice = input.nextInt();
-		
-		if(choice == 1)
-		{
-			result = "You've chosen the park view for 150,000$";
-		}
-		else if(choice == 2)
-		{
-			result = "You've chosen the golf course view for 170,000$";
-		}
-		else if(choice == 3)
-		{
-			result = "You've chosen the lake view for 210,000$";
-		}
-		else
-			result = "That is not a choice";
-		displayChoice(result);
+		do {
+			System.out.println("Pick a number 1-3");
+			choice = input.nextInt();
+			
+			if(choice == 1)
+			{
+				result = "You've chosen the park view for 150,000$";
+			}
+			else if(choice == 2)
+			{
+				result = "You've chosen the golf course view for 170,000$";
+			}
+			else if(choice == 3)
+			{
+				result = "You've chosen the lake view for 210,000$";
+			}
+			else
+				result = "That is not a choice";
+			displayChoice(result);
+		}while();	
 	}
 	public static void displayChoice(String result)
 	{
